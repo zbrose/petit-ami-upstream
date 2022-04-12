b1f2ea6688da549ed7e5785960bef4632d8bb959
zbrose <zrbrose@gmail.com>
2022-04-12 09:46:23 -0700

    Initial Commit

---
+++ petit-ami/petit_amiApp.swift
@@ -0,0 +1,17 @@
+//
+//  petit_amiApp.swift
+//  petit-ami
+//
+//  Created by Zach Brose on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct petit_amiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
