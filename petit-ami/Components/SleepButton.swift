88640d99b2a51fd98045daad554af2039342848c
tristonpalacios <triston.j.palacios@gmail.com>
2022-04-14 15:23:35 -0700

    New Button Layout

--- petit-ami/Components/SleepButton.swift
+++ petit-ami/Components/SleepButton.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct SleepButton: View {
     var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 50)
-                .foregroundColor(Color(hue: 0.000, saturation: 0.000, brightness: 0.908))
+       ZStack {
             
-            Image("Sleep")
+            Circle() // Our button
+                .fill(Color(.gray))
+                            .frame(width: 50, height: 50)
+                            .shadow(color: .black.opacity(0.3), radius: 0, x: 0, y: 8)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color(.black), lineWidth: 7)
+                                    .frame(width: 60, height: 60)
+                                    .offset(x: 0, y: -5)
+                                    .mask(
+                                        Circle()
+                                            .frame(width: 50, height: 50)
+                                    )
+                            )
+            
+
+            
+            Image("SleepW")
                 .resizable()
-                .padding(.all)
-                .padding(.top,5)
-                .frame(width: 80, height: 80)
+                .padding()
+                .frame(width: 70, height: 70)
+                
+
+                
         }
         .frame(height:50)
     }
