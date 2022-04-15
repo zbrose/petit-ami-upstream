970930e434f54bbf23ae4d90f0c4461c9338f2d3
Zach Brose <97310057+zbrose@users.noreply.github.com>
2022-04-14 20:13:52 -0700

    adds instructions sheet

--- petit-ami/Views/ContentView.swift
+++ petit-ami/Views/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var realmManager = RealmManager()
     @State private var showNameAmiView = false
+    @State private var showInstructions = false
     
     var body: some View {
-       
+    
+        
         (realmManager.amis.count > 0 ? AnyView(ButtonBar()) : AnyView(NameAmiView().environmentObject(realmManager)))
 //            List {
 //                ForEach(realmManager.amis, id: \.id) { ami in
@@ -33,7 +35,9 @@ struct ContentView: View {
         .sheet(isPresented: $showNameAmiView) {
             NameAmiView()
                 .environmentObject(realmManager)
-            
+        .sheet(isPresented: $showInstructions){
+            InstructionsView()
+        }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
