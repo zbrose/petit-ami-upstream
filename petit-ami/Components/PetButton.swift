d675613b1debebeb6af50ce1d72cab753078d5b6
tristonpalacios <triston.j.palacios@gmail.com>
2022-04-14 23:26:26 -0700

    Added .gif functionality and Heart and Bolt images

--- petit-ami/Components/PetButton.swift
+++ petit-ami/Components/PetButton.swift
@@ -12,12 +12,31 @@ struct PetButton: View {
     
     var body: some View {
         ZStack {
-            Circle()
-                .frame(width: 50)
-                .foregroundColor(Color(hue: 0.000, saturation: 0.000, brightness: 0.908))
             
-            Image(systemName: "hand.raised.fill")
-                .font(.system(size: 30.0, weight: .bold))
+            Circle() // Our button
+                .fill(Color(.gray))
+                            .frame(width: 50, height: 50)
+                            .shadow(color: .black.opacity(0.3), radius: 0, x: 0, y: 8)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color(.black), lineWidth: 7)
+                                    .frame(width: 60, height: 60)
+                                    .offset(x: 0, y: -5)
+                                    .mask(
+                                        Circle()
+                                            .frame(width: 50, height: 50)
+                                    )
+                            )
+            
+
+            
+            Image("HeartW")
+                .resizable()
+                .padding()
+                .frame(width: 80, height: 70)
+                
+
+                
         }
         .frame(height:50)
     }
